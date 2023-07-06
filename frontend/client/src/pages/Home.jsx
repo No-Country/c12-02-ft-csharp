e15a632d8a1a3733ac18cb560d191a6afaa75b08
Rumer Tovar <rumertovar7474@gmail.com>
2023-07-06 11:17:28 -0500

    cleaning home page

--- frontend/client/src/pages/Home.jsx
+++ frontend/client/src/pages/Home.jsx
@@ -1,12 +1,7 @@
-import Footer from "../components/Footer";
-import { Navbar } from "../components/Navbar";
-
 function Home() {
   return (
     <>
-      <div>
-        Home
-      </div>
+      <div>Home</div>
     </>
   );
 }
